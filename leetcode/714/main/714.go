d7589157393bcf7c7cef2db95e7debb60a680e7c
BooM77777 <3293612954@qq.com>
2020-12-17 09:46:55 +0800

    2020-12-17 + leetcode714

---
+++ leetcode/714/main/714.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	prices := [...]int{0: 1, 1: 3, 2: 2, 3: 8, 4: 4, 5: 9}
+	fmt.Println(maxProfit(prices, 2))
+}
+
+func maxProfit(prices []int, fee int) int {
+
+	var max func(a, b int) int
+	max = func(a, b int) int {
+		if a > b {
+			return a
+		}
+		return b
+	}
+
+	var empty = 0
+	var busy = -prices[0]
+
+	for _, price := range prices {
+		empty = max(empty, busy+price-fee)
+		busy = max(busy, empty-price)
+	}
+
+	return empty
+}
