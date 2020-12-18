c5f3f42c4f3e6bcabc9cb1d1f0b81cc20e85a1b1
BooM77777 <3293612954@qq.com>
2020-12-18 09:26:39 +0800

    2020-12-18 : +leetcode_389

---
+++ leetcode/389/main/389.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var s = "ihwrdisrhgxwbfevrxbtzgsywhnzleueadikniwyuasflpsviobwvsmydmyzppqjlmzakpbnouyttdigkcdzypvcqxbttmblttehgjlnpjwpzoprntifysfatjboasottnkpyyvmdcafpjicfpgmbwqdsaxdmmdmupnwhkpxixpdwmczntqtushemvavofszomtsrafzmxctpidjadwcwggdbyliqmcvuwscryfsvlvfrhfphmxvcnytbctomicwdwjjmdhmcqtnlqgixxdyjydhwnftkobotbhsgykawhtvnkxoykwkgvtqioqoiilergxlpuujabiug"
+	var t = "gyptmtjntxlusjhbzkbgowslthwtytdnflsyfsgfytzrodatykdyvgsmvxsuemijitvodmwrrqmcabhwzyoouorfckhisjpduoxvtmttzvwmicdxsovsabmpcpppzycuwbmpihmxadmvkkwerimhgwdwdtvqwbwtetppkpkbcaifuqbenagycdqatklciaczcpglxmvfaqnwpnssdmnhcmifeyndzttvypwlgpttvhswoiijybchbvzklgngqziyaczowgwiufqyhxxdqjrxolddgnmriijopdsikqwtyhplhubrljfjanexxyfvjmudxsomnfbafntpib"
+	fmt.Println(findTheDifference(s, t))
+}
+
+func findTheDifference(s string, t string) byte {
+
+	var l = make([]int, 26)
+
+	for _, c := range s {
+		l[c-'a']++
+	}
+
+	for _, c := range t {
+		l[c-'a']--
+	}
+
+	for i, v := range l {
+		if v == 0 {
+			continue
+		}
+		return byte(i) + 'a'
+	}
+	return '0'
+}
